0e2234c669f2e7e42f9e1277dd038bd067224539
mscoutermarsh <coutermarsh.mike@gmail.com>
2011-03-20 00:00:56 -0400

    interacts with api

--- src/org/seniorProject/Pet.java
+++ src/org/seniorProject/Pet.java
@@ -3,6 +3,9 @@ package org.seniorProject;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,8 +31,9 @@ import org.apache.http.protocol.HTTP;
 
 public class Pet extends Activity {
 	private String api_key;
-	private String email;
+	private String email ="coutermarsh.mike@gmail.com";
 	public static final String PREFS_NAME = "LocalPetData";
+	private TextView text_age,text_hunger,text_mood,text_cleanliness;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -40,7 +44,54 @@ public class Pet extends Activity {
 		// Restore pet data
 		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
 		api_key = settings.getString("key", null);
-		email = settings.getString("email", null);
+		//email = settings.getString("email", null);
+
+		// Setup Textviews
+		text_age = new TextView(this);
+		text_hunger = new TextView(this);
+		text_mood = new TextView(this);
+		text_cleanliness = new TextView(this);
+
+		text_age = (TextView) findViewById(R.id.age);
+		text_hunger = (TextView) findViewById(R.id.hunger);
+		text_cleanliness = (TextView) findViewById(R.id.cleanliness);
+		text_mood = (TextView) findViewById(R.id.mood);
+		
+		// Setup buttons
+		final Button button_feed = (Button) findViewById(R.id.feed);
+		final Button button_play = (Button) findViewById(R.id.play);
+		final Button button_clean = (Button) findViewById(R.id.clean);
+		
+		 button_feed.setOnClickListener(new View.OnClickListener() {
+	            public void onClick(View v) {
+	            	try {
+						executeHttpPost(api_key, "feed", email, null);
+						updatePetData();
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+	            }
+	        });
+		 button_play.setOnClickListener(new View.OnClickListener() {
+	            public void onClick(View v) {
+	            	try {
+						executeHttpPost(api_key, "play", email, null);
+						updatePetData();
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+	            }
+	        });
+		 button_clean.setOnClickListener(new View.OnClickListener() {
+	            public void onClick(View v) {
+	            	try {
+						executeHttpPost(api_key, "clean", email, null);
+						updatePetData();
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+	            }
+	        });
 
 		// if no key... create new pet
 		if (api_key == null) {
@@ -49,8 +100,7 @@ public class Pet extends Activity {
 				String name = "Zinkus";
 				String color = "Blue";
 
-				api_key = executeHttpPost(null, "", "email=" + email + "&name="
-						+ name + "&color=" + color);
+				api_key = executeHttpPost(null, "",email, "name=" + name + "&color=" + color);
 				// Save API Key
 				SharedPreferences.Editor editor = settings.edit();
 				editor.putString("key", api_key);
@@ -62,27 +112,45 @@ public class Pet extends Activity {
 				e.printStackTrace();
 			}
 		}
+
+		// Update pet data
+		updatePetData();
+	}
+	
+	// update text boxes with pet data
+	public void updatePetData() {
+		try {
+
+			text_age.setText("Age: " + executeHttpGet(api_key, "age"));
+			text_hunger.setText("Hunger: " + executeHttpGet(api_key, "hunger"));
+			text_cleanliness.setText("Cleanliness: "
+					+ executeHttpGet(api_key, "cleanliness"));
+			text_mood.setText("Mood: " + executeHttpGet(api_key, "mood"));
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	// get Data for specific pet (key)
-	public String executeHttpGet(String key, String action) throws Exception {
+	public String executeHttpGet(String Key, String action) throws Exception {
 		BufferedReader in = null;
-		String URI = "mcqu.heroku.com/pet/" + key + "/action";
 		try {
-			HttpClient client = new DefaultHttpClient();
-			HttpGet request = new HttpGet();
-			request.setURI(new URI(URI));
-			HttpResponse response = client.execute(request);
-			in = new BufferedReader(new InputStreamReader(response.getEntity()
-					.getContent()));
-			StringBuffer sb = new StringBuffer("");
-			String line = "";
-			String NL = System.getProperty("line.separator");
-			while ((line = in.readLine()) != null) {
-				sb.append(line + NL);
+			String URI = "http://mcqu.heroku.com/pet";
+			if (Key != null) {
+				URI = URI + "/" + Key;
 			}
-			in.close();
-			return sb.toString();
+			if (action != null) {
+				URI = URI + "/" + action;
+			}
+
+			InputStream content = null;
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpGet httpGet = new HttpGet(URI);
+			// Execute HTTP Get Request
+			HttpResponse response = httpclient.execute(httpGet);
+			return getResponseBody(response);
 		} finally {
 			if (in != null) {
 				try {
@@ -95,16 +163,22 @@ public class Pet extends Activity {
 	}
 
 	// execute post
-	public String executeHttpPost(String Key, String action, String params)
+	public String executeHttpPost(String Key, String action, String email, String params)
 			throws Exception {
 		BufferedReader in = null;
 		try {
 			String URI = "http://mcqu.heroku.com/pet";
 			if (Key != null) {
-				URI = URI + Key;
+				URI = URI + "/" + Key;
+			}
+			if (action != null){
+				URI = URI + "/" + action;
+			}
+			if (email != null) {
+				URI = URI + "?email=" + email;
 			}
 			if (params != null) {
-				URI = URI + "?" + params;
+				URI = URI + "&" + params;
 			}
 
 			InputStream content = null;
